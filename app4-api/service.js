55daa107498c299d8165f87b2b9cb614467b6b65
Kiwicannon <stevencannon01@gmail.com>
2017-02-10 10:45:41 -0700

    run4 do it

--- app4-api/service.js
+++ app4-api/service.js
@@ -1,4 +1,15 @@
-angular.module('apiApp').service('mainSvc',function(){
+angular.module('apiApp').service('mainSvc',function($http){
     this.test1='service is working';
-    this.function()
+
+    this.getData= function(){
+        return $http({
+            method:"GET",
+            url:'http://swapi.co/api/people'
+        }).then(function(response){
+            if(response.status===200){
+                console.log(response.data.results)
+                return response.data.results
+            }return "Error" + response.status
+        })
+    }
 })
