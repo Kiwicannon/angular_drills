dd146312138ffaa6ca25fdca9b2ebdb759050c4b
Kiwicannon <stevencannon01@gmail.com>
2017-02-24 11:54:47 -0700

    1

--- app2-an_array/service.js
+++ app2-an_array/service.js
@@ -102,7 +102,10 @@ var myData = [
 ];
 
 
-
+angular.module('arrayApp').service('mainSvc', function(){
+	this.test1 = 'service is working'
+	this.getData = myData
+})
 
 
 
---
+++ app3-filtering/controller.js
@@ -0,0 +1,6 @@
+angular.module('app').controller('mainCtrl', function($scope, mainSvc){
+    $scope.test = 'controller is working'
+    $scope.test1 = mainSvc.test1
+    $scope.employees = mainSvc.getData
+    console.log($scope.employees)
+})
